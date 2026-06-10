a6de7e587238284b37832d4f91e731dd006dcb61
Takumi <legottmax@gmail.com>
2026-06-10 20:21:33 +0900

    作業中の退避

--- frontend/super_manager/lib/pages/store_map.dart
+++ frontend/super_manager/lib/pages/store_map.dart
@@ -1,17 +1,32 @@
-
-
 import 'package:flutter/material.dart';
 
-class StoreMap extends StatelessWidget{
+class StoreMap extends StatelessWidget {
   const StoreMap({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Row(
-        children: [
-          Text("test")
-        ],
+      body: SizedBox(
+        width: 300,
+        child: Column(
+          children: [
+            Expanded(
+              child: Scrollbar(
+                child: ListView.builder(
+                  primary: true,
+                  itemCount: 100,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text('ナショナル $index号店'),
+                      subtitle: Text('大阪市'),
+                      trailing: Text('なしょなるだよ'),
+                    );
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
