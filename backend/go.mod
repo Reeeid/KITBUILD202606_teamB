5dd92e98b64faa92fb2e5060723ec4e3e5633ceb
tadanobutaaaaa <youyah1004@gmail.com>
2026-06-30 00:18:20 +0900

    環境変数の参照方法を変更

--- backend/go.mod
+++ backend/go.mod
@@ -5,7 +5,6 @@ go 1.26.4
 require (
 	github.com/gin-gonic/gin v1.12.0
 	github.com/go-sql-driver/mysql v1.10.0
-	github.com/joho/godotenv v1.5.1
 	gorm.io/driver/mysql v1.6.0
 	gorm.io/gorm v1.31.1
 )
--- backend/internal/db/mysql.go
+++ backend/internal/db/mysql.go
@@ -2,12 +2,10 @@ package db
 
 import (
 	"database/sql"
-	"fmt"
 	"log"
 	"os"
 
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 
@@ -19,7 +17,6 @@ var DB *gorm.DB
 // データベース接続の初期化関数
 func InitDB() *sql.DB {
 	var err error
-	loadEnv()
 
 	DB, err = gorm.Open(mysql.Open(os.Getenv("DATABASE_URL")), &gorm.Config{})
 	if err != nil {
@@ -29,16 +26,4 @@ func InitDB() *sql.DB {
 	DB.AutoMigrate(&models.Category{}, &models.Product{}, &models.Store{})
 	sql, _ := DB.DB()
 	return sql
-}
-
-// .envファイルから環境変数を読み込む関数
-func loadEnv() {
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	databaseUrl := os.Getenv("DATABASE_URL")
-
-	fmt.Println("DATABASE_URL:", databaseUrl)
 }
